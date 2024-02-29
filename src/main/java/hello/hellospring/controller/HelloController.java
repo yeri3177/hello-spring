2a4f7cfd4db31dd9ddeea37940411f81025bc4a5
yeri3177 <yeri3177@gmail.com>
2024-02-29 16:20:49 +0900

    섹션 2-3. API

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,40 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    /**
+     * @ResponseBody 를 사용하면 뷰 리졸버( viewResolver )를 사용하지 않음
+     * 대신에 HTTP의 BODY에 문자 내용을 직접 반환(HTML BODY TAG를 말하는 것이 아님)
+     */
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    /**
+     * @ResponseBody 를 사용
+     * HTTP의 BODY에 문자 내용을 직접 반환
+     * viewResolver 대신에 HttpMessageConverter 가 동작
+     * 기본 문자처리: StringHttpMessageConverter
+     * 기본 객체처리: MappingJackson2HttpMessageConverter
+     * byte 처리 등등 기타 여러 HttpMessageConverter가 기본으로 등록되어 있음
+     */
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello {
+        private String name;
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
